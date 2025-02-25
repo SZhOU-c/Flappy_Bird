74a414106e976df8a155b01f16c352f8172a1e4d
kUin1 <joeqvq@gmail.com>
2025-02-25 17:24:35 -0600

    game setup

---
+++ flappy_bird.py
@@ -0,0 +1,42 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+
+class Bird:
+	IMGS = BIRD_IMGS
+	MAX_ROTATION = 25
+	ROT_VEL = 20
+	ANIMATION_TIME = 5
+
+	def _init_(self,x,y):
+		self.x = x 
+		self.y = y 
+		self.tilt = 0 
+		self.tick_count = 0 
+		self.vel = 0 
+		self.height = self.y 
+		self.img_count = 0 
+		self.img = self.IMGS[0]
+
+	def jump(self):
+		self.vel = -10.5
+		self.tick_count = 0
+		self.height = self.y 
+
+
+	def move(self):
+
+
+while True:
+	bird.move()
